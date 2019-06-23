7e43800327a8a5c68ca4ccd246c951b8895280b8
xxxThinking <43473278+xxxThinking@users.noreply.github.com>
2019-06-23 23:23:57 +0800

    Change_2

---
+++ BollGameT1/src/bollGame/TimeCount.java
@@ -0,0 +1,26 @@
+package bollGame;
+
+public class TimeCount {
+	long initMsec;
+	long curMsec;
+	long res;
+	int addnum;
+	
+	public TimeCount(){
+		initMsec = System.currentTimeMillis();
+		res = 0;
+	}
+	
+	//重写update
+	public void update(){
+		curMsec=System.currentTimeMillis();
+		res = curMsec-initMsec;	
+	}
+	
+	public String showTime(){
+		String time ;
+		time = String.format("%.1f″", res/1000.0);
+		return time;
+	}
+	
+}
